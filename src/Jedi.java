6030d10911352fad13e3709847e1a63f0ae50733
BT_2N2_22 <sample@bths.edu>
2024-03-12 13:50:05 -0400

    Created Sith, Jedi, and the main game class

---
+++ src/Jedi.java
@@ -0,0 +1,5 @@
+public class Jedi {
+    public Jedi(String name, int midichlorianCount, double health) {{
+        super(name, midichlorianCount, health);
+    }
+}
---
+++ src/StarWarsGame.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class StarWarsGame {
+    Scanner scan = new Scanner(System.in);
+    public StarWarsGame() {
+        greetings();
+        start();
+    }
+
+    public void greetings() {
+        System.out.println("Hello young traveler! State your name: ");
+        String name = scan.nextLine();
+        System.out.println("Are you part of the dark side or the light side?");
+        String side = scan.nextLine();
+        if () {
+
+        }
+    }
+
+    public void start() {
+
+    }
+}
