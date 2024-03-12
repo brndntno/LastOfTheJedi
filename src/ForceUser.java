245e3d8eddd7ab227430726c6cad6448605852b9
BT_2N2_21 <sample@bths.edu>
2024-03-12 13:16:18 -0400

    initial commit

---
+++ src/ForceUser.java
@@ -0,0 +1,16 @@
+public class ForceUser {
+    String name;
+    int midichlorianCount;
+    public ForceUser(String name, int midichlorianCount) {
+        this.name = name;
+        this.midichlorianCount = midichlorianCount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMidichlorianCount() {
+        return midichlorianCount;
+    }
+}
