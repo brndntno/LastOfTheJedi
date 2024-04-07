08ceec8ad5a830b9f30789bcb31e4ed53adcae6d
Brandon Wong <brandonfn2007@gmail.com>
2024-04-07 17:28:49 -0400

    he pushed smth

--- src/HelloDolly.java
+++ src/HelloDolly.java
@@ -8,5 +8,6 @@ public class HelloDolly {
         System.out.println("a");
         System.out.println("while");
         System.out.println("he fixed smth");
+        System.out.println("wow");
     }
 }
